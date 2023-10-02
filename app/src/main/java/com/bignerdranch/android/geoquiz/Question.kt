0380feacff416c4846d3fff90b58c9182f4b5de5
Gerardo Solis <gsolis5114@gmail.com>
2023-10-02 15:27:43 -0700

    Added back button and text onClickListener

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.annotation.StringRes
+
+// resourceID for the question text
+// question text and the question answer (true or false)
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
