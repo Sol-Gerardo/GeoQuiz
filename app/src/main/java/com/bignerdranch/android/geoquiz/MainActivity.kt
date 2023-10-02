3701498cc655573e7cc1d8e53aadf2b48090e300
Gerardo Solis <gsolis5114@gmail.com>
2023-10-02 11:28:24 -0700

    Added Snackbar feature

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,18 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener{ view: View ->
             // We pass MainActivity as the Context argument.
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT
+                Snackbar.LENGTH_SHORT
             ).show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
+            Snackbar.make(
+                view,
+                R.string.correct_toast,
+                Snackbar.LENGTH_SHORT
             ).show()
         }
     }
